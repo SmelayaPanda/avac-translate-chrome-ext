ac1a7cc76b7daedeee78399e79b813c0ad980489
panda <SmelayaPandaGM@gmail.com>
2017-08-13 00:35:35 +0700

    -

--- js/content.js
+++ js/content.js
@@ -1,15 +1,15 @@
 window.onload = function () {
     var http = new XMLHttpRequest();
-    var url = "Translator";
+    var url = "http://localhost:8080/avac";
     var text = document.body.textContent;
     var params = "text=" + text;
-    http.open("POST", url, true);
+    http.open("GET", url);
     http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
     http.onreadystatechange = function () {
         if (http.readyState === 4 && http.status === 200) {
-            alert(http.responseText);
+            console.log(http.responseText);
         }
     };
-    http.send(params);
+    http.send();
 };
