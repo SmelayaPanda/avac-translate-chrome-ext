c191b00c22cf8575f9d2fad7a89e06241307fd32
panda <SmelayaPandaGM@gmail.com>
2017-08-15 00:04:00 +0700

    working

--- js/content.js
+++ js/content.js
@@ -1,15 +1,33 @@
 window.onload = function () {
-    var http = new XMLHttpRequest();
-    var url = "http://localhost:8080/avac";
-    var text = document.body.textContent;
-    var params = "text=" + text;
-    http.open("GET", url);
-    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    const url = "http://localhost:8080/avac";
+    const req = new XMLHttpRequest();
+    const params = "goto=" + document.URL;
+    req.open("POST", url, true);
+    req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
-    http.onreadystatechange = function () {
-        if (http.readyState === 4 && http.status === 200) {
-            console.log(http.responseText);
+    req.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            let myDictionary = JSON.parse(this.responseText);
+            console.log(myDictionary);
+
+            console.log("Start");
+
+            let paragraphs = document.getElementsByTagName("p");
+            for (let i = 0; i < paragraphs.length; i++) {
+                let text = paragraphs[i].textContent;
+
+                for (let key in myDictionary) {
+                    if (myDictionary.hasOwnProperty(key)) {
+                        text = text.replace(key,
+                            `${key} [ <span style="color: green"><i>${myDictionary[key]}</i></span>]`);
+                    }
+                }
+
+                paragraphs[i].innerHTML = text;
+            }
+            console.log("Complete");
+
         }
     };
-    http.send();
+    req.send(params);
 };
