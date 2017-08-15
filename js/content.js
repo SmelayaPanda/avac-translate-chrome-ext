9f9eff981b7b22f9a095054651c5adaabd19554a
panda <SmelayaPandaGM@gmail.com>
2017-08-15 13:56:12 +0700

    working

--- js/content.js
+++ js/content.js
@@ -1,3 +1,28 @@
+function translateText() {
+    let myDictionary = JSON.parse(this.responseText);
+    console.log(myDictionary);
+
+    console.log("Start");
+
+    let paragraphs = document.getElementsByTagName("p");
+    for (let i = 0; i < paragraphs.length; i++) {
+        let text = paragraphs[i].textContent;
+
+        for (let key in myDictionary) {
+            if (myDictionary.hasOwnProperty(key)) {
+                text = text.replace(" " + key + " ",
+                    ` ${key} [ <span style="color: green">
+                                  <i>${myDictionary[key]}</i>
+                              </span>
+                             ] `);
+            }
+        }
+
+        paragraphs[i].innerHTML = text;
+    }
+    console.log("Complete");
+}
+
 window.onload = function () {
     const url = "http://localhost:8080/avac";
     const req = new XMLHttpRequest();
@@ -7,26 +32,7 @@ window.onload = function () {
 
     req.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
-            let myDictionary = JSON.parse(this.responseText);
-            console.log(myDictionary);
-
-            console.log("Start");
-
-            let paragraphs = document.getElementsByTagName("p");
-            for (let i = 0; i < paragraphs.length; i++) {
-                let text = paragraphs[i].textContent;
-
-                for (let key in myDictionary) {
-                    if (myDictionary.hasOwnProperty(key)) {
-                        text = text.replace(key,
-                            `${key} [ <span style="color: green"><i>${myDictionary[key]}</i></span>]`);
-                    }
-                }
-
-                paragraphs[i].innerHTML = text;
-            }
-            console.log("Complete");
-
+            translateText.call(this);
         }
     };
     req.send(params);
