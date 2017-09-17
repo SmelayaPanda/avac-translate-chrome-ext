2a4c27eeb62047f37f64cbf75440bbaefb9b4546
panda <SmelayaPanda@mail.ru>
2017-09-17 16:21:40 +0700

    fix if first extension load

--- js/content.js
+++ js/content.js
@@ -1,153 +1,161 @@
 /** --------------
  * Listen popup.js
  */
-
-chrome.storage.sync.get( 'onLoadCheckBox', function ( obj )
+window.onload = function ()
 {
-    if( obj.onLoadCheckBox )
+
+    chrome.storage.sync.get('onLoadCheckBox', function (obj)
     {
-        let level;
-        let langFrom;
-        let langTo;
-
-        chrome.storage.sync.get( 'rangeInput', function ( obj )
+        if (obj.onLoadCheckBox)
         {
-            level = obj.rangeInput;
-            chrome.storage.sync.get( 'langFrom', function ( obj )
-            {
-                langFrom = obj.langFrom;
-                chrome.storage.sync.get( 'langTo', function ( obj )
-                {
-                    langTo = obj.langTo;
-                    avacPost( level, langFrom, langTo );
-                } );
-            } );
-        } );
-    }
-} );
+            let level;
+            let langFrom;
+            let langTo;
 
-chrome.runtime.onMessage.addListener(
+            chrome.storage.sync.get('rangeInput', function (obj)
+            {
+                obj.rangeInput ? level = obj.rangeInput : level = 0;
+                chrome.storage.sync.get('langFrom', function (obj)
+                {
+                    obj.langFrom ? langFrom = obj.langFrom : 'eng';
+                    chrome.storage.sync.get('langTo', function (obj)
+                    {
+                        obj.langTo ? langTo = obj.langTo : 'eng';
+                        avacPost(level, langFrom, langTo);
+                    });
+                });
+            });
+        }
+    });
+
+    chrome.runtime.onMessage.addListener(
         msgObj =>
         {
-            let params = JSON.parse( msgObj );
-            avacPost( params.level, params.langFrom, params.langTo );
-        } );
+            let params = JSON.parse(msgObj);
+            avacPost(params.level, params.langFrom, params.langTo);
+        });
+};
+
 /** ------------------------------------------------------------------ */
-function avacPost( level, langFrom, langTo )
+function avacPost(level, langFrom, langTo)
 {
     createAvacFooter();
     const url = "https://panda.jelastic.regruhosting.ru/avac/";
     const req = new XMLHttpRequest();
     const params =
-            "goto=" + encodeURIComponent( document.URL ) + "&" +
-            "level=" + level + "&" +
-            "langFrom=" + langFrom + "&" +
-            "langTo=" + langTo;
+        "goto=" + encodeURIComponent(document.URL) + "&" +
+        "level=" + level + "&" +
+        "langFrom=" + langFrom + "&" +
+        "langTo=" + langTo;
 
-    console.log( params );
-    req.open( "POST", url, true );
-    req.setRequestHeader( "Content-type", "application/x-www-form-urlencoded" );
+    console.log(params);
+    req.open("POST", url, true);
+    req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
     req.onreadystatechange = function ()
     {
         let myDictionary;
-        if( this.readyState === 4 && this.status === 200 )
+        if (this.readyState === 4 && this.status === 200)
         {
-            myDictionary = JSON.parse( this.responseText );
-            console.log( " Dictionary size = " + myDictionary.size );
-            if( document.readyState === 'complete' )
+            myDictionary = JSON.parse(this.responseText);
+            if (document.readyState === 'complete')
             {
-                translateText( myDictionary );
+                console.log(" Dictionary size = " + myDictionary.size);
+                translateText(myDictionary);
             }
         }
     };
-    req.send( params );
+    req.send(params);
 }
+
 /** ------------------------------------------------------------------ */
-function translateText( myDictionary )
+function translateText(myDictionary)
 {
-    console.log( "Start translating ... " );
-    if( document.getElementsByClassName( "avacWord" ) )
+    console.log("Start translating ... ");
+    if (document.getElementsByClassName("avacWord"))
     {
-        removeElementsByClass( "avacWord" )
+        removeElementsByClass("avacWord")
     }
     let words;
     let text;
-    let paragraphs = document.getElementsByTagName( "p" );
-    for( let i = 0; i < paragraphs.length; i++ )
+    let paragraphs = document.getElementsByTagName("p");
+    for (let i = 0; i < paragraphs.length; i++)
     {
         text = paragraphs[i].textContent;
 
         text = text
-                .replace( text.charAt( 0 ), ' ' + text.charAt( 0 ) ) // Begin of the paragraph
-                .replace( text.charAt( text.length - 1 ), text.charAt( text.length - 1 ) + ' ' )
-                .replace( /\./g, ' .' )
-                .replace( /,/g, ' ,' )
-                .replace( /:/g, ' :' )
-                .replace( /;/g, ' ;' )
-                .replace( /!/g, ' !' )
-                .replace( /\?/g, ' ?' )
-                .replace( /\(/g, ' ;' )
-                .replace( /\)/g, ' ;' )
-                .replace( /\s+/g, '  ' );
+            .replace(text.charAt(0), ' ' + text.charAt(0)) // Begin of the paragraph
+            .replace(text.charAt(text.length - 1), text.charAt(text.length - 1) + ' ')
+            .replace(/\./g, ' .')
+            .replace(/,/g, ' ,')
+            .replace(/:/g, ' :')
+            .replace(/;/g, ' ;')
+            .replace(/!/g, ' !')
+            .replace(/\?/g, ' ?')
+            .replace(/\(/g, ' ;')
+            .replace(/\)/g, ' ;')
+            .replace(/\s+/g, '  ');
 
         words = text
-                .replace( /[.,!;:\?()]/g, " " )
-                .replace( /\s+/g, ' ' )
-                .split( ' ' );
+            .replace(/[.,!;:?()]/g, " ")
+            .replace(/\s+/g, ' ')
+            .split(' ');
 
-        for( let w in words )
+        for (let w in words)
         {
-            text = text.replace( ' ' + words[w] + ' ', `<span class="avacMainWord ___${words[w].toLowerCase()}"> ${words[w]} </span> ` );
+            text = text.replace(' ' + words[w] + ' ', `<span class="avacMainWord ___${words[w].toLowerCase()}"> ${words[w]} </span> `);
         }
         paragraphs[i].innerHTML = text;
     }
 
     let classWords;
-    for( let key in myDictionary )
+    for (let key in myDictionary)
     {
-        classWords = document.getElementsByClassName( "___" + key );
-        for( cw in classWords )
+        classWords = document.getElementsByClassName("___" + key);
+        for (cw in classWords)
         {
             classWords[cw].innerHTML = `${classWords[cw].innerText} <span class='avacWord'>[&nbsp${myDictionary[key]}&nbsp]</span>`;
         }
     }
 
-    let avacWords = document.querySelectorAll( '[class*=___]' );
-    for( let av in avacWords )
+    let avacWords = document.querySelectorAll('[class*=___]');
+    for (let av in avacWords)
     {
         avacWords[av].onclick = function ()
         {
-            document.getElementById( 'translatedAvacWord' ).innerText = avacWords[av].textContent.substring( 0, avacWords[av].textContent.indexOf( '[' ) - 2 ).trim().toUpperCase();
+            document.getElementById('translatedAvacWord').innerText = avacWords[av].textContent.substring(0, avacWords[av].textContent.indexOf('[') - 2).trim().toUpperCase();
         }
     }
-    console.log( "Complete!" );
+    console.log("Complete!");
 }
+
 /** ------------------------------------------------------------------ */
-function removeElementsByClass( className )
+function removeElementsByClass(className)
 {
-    let elements = document.getElementsByClassName( className );
-    while( elements.length > 0 )
+    let elements = document.getElementsByClassName(className);
+    while (elements.length > 0)
     {
-        elements[0].parentNode.removeChild( elements[0] );
+        elements[0].parentNode.removeChild(elements[0]);
     }
 }
+
 /** ------------------------------------------------------------------ */
 function createAvacFooter()
 {
-    let footer = document.createElement( 'footer' );
+    let footer = document.createElement('footer');
     footer.id = 'avacFooter';
 
     footer.innerHTML =
-            `<div id="avacFooterContent">
+        `<div id="avacFooterContent">
                 <div id="translatedAvacWord"> 
                     Welcome!
                 </div>
             </div>`;
-    document.body.appendChild( footer );
+    document.body.appendChild(footer);
     document.body.style.marginBottom = '100px';
 
 }
+
 /** ------------------------------------------------------------------ */
 
 
--- js/popup.js
+++ js/popup.js
@@ -3,141 +3,143 @@ window.onload = function ()
     /** ------------
      * HTML elements
      */
-    let langFromBox = document.getElementById( 'langFrom' );
-    let langToBox = document.getElementById( 'langTo' );
-    let rangeInput = document.getElementById( 'avacLevel' );
-    let displayedLevel = document.getElementById( "rangeValue" );
-    let translate_btn = document.getElementById( "translate_btn" );
-    let onLoadCheckBox = document.getElementById( "safeTranslateMode" );
+    let langFromBox = document.getElementById('langFrom');
+    let langToBox = document.getElementById('langTo');
+    let rangeInput = document.getElementById('avacLevel');
+    let displayedLevel = document.getElementById("rangeValue");
+    let translate_btn = document.getElementById("translate_btn");
+    let onLoadCheckBox = document.getElementById("safeTranslateMode");
 
     /** ---------------------------
      * Setting Chrome storage value
      */
     langFromBox.onchange = function ()
     {
-        chrome.storage.sync.set( {'langFrom': langFromBox.value} );
+        chrome.storage.sync.set({'langFrom': langFromBox.value});
     };
     langToBox.onchange = function ()
     {
-        chrome.storage.sync.set( {'langTo': langToBox.value} );
+        chrome.storage.sync.set({'langTo': langToBox.value});
     };
     rangeInput.onchange = function ()
     {
-        chrome.storage.sync.set( {'rangeInput': rangeInput.value} );
-        chrome.storage.sync.set( {'displayedLevel': displayedLevel.innerText} );
+        chrome.storage.sync.set({'rangeInput': rangeInput.value});
+        chrome.storage.sync.set({'displayedLevel': displayedLevel.innerText});
     };
     onLoadCheckBox.onchange = function ()
     {
-        if( onLoadCheckBox.checked )
+        if (onLoadCheckBox.checked)
         {
-            chrome.storage.sync.set( {'onLoadCheckBox': true} );
+            chrome.storage.sync.set({'onLoadCheckBox': true});
         }
         else
         {
-            chrome.storage.sync.set( {'onLoadCheckBox': false} );
+            chrome.storage.sync.set({'onLoadCheckBox': false});
         }
     };
     /** ---------------------------
      * Getting Chrome storage value
      */
-    chrome.storage.sync.get( 'langFrom', function ( obj )
+    chrome.storage.sync.get('langFrom', function (obj)
     {
         langFromBox.value = obj.langFrom ? obj.langFrom : 'eng';
-    } );
-    chrome.storage.sync.get( 'langTo', function ( obj )
+    });
+    chrome.storage.sync.get('langTo', function (obj)
     {
         langToBox.value = obj.langTo ? obj.langTo : 'eng';
-    } );
-    chrome.storage.sync.get( 'rangeInput', function ( obj )
+    });
+    chrome.storage.sync.get('rangeInput', function (obj)
     {
         rangeInput.value = obj.rangeInput ? obj.rangeInput : 0;
-    } );
-    chrome.storage.sync.get( 'displayedLevel', function ( obj )
+    });
+    chrome.storage.sync.get('displayedLevel', function (obj)
     {
         displayedLevel.innerText = obj.displayedLevel ? obj.displayedLevel : 'WELCOME TO AVAC! ';
-    } );
-    chrome.storage.sync.get( 'onLoadCheckBox', function ( obj )
+    });
+    chrome.storage.sync.get('onLoadCheckBox', function (obj)
     {
-        if( obj.onLoadCheckBox )
+        if (obj.onLoadCheckBox)
         {
             onLoadCheckBox.checked = true;
         }
-    } );
+    });
     /** ---------------------------------------------------- */
-    rangeInput.addEventListener( 'input', function ()
+    rangeInput.addEventListener('input', function ()
     {
-        if( this.value < 20 && displayedLevel.innerHTML !== "BEGINNER" )
+        if (this.value < 20 && displayedLevel.innerHTML !== "BEGINNER")
         {
-            fadeinText( displayedLevel, "BEGINNER" );
+            fadeinText(displayedLevel, "BEGINNER");
         }
-        else if( this.value >= 20 && this.value < 40 && displayedLevel.innerHTML !== "PRE INTERMEDIATE" )
+        else if (this.value >= 20 && this.value < 40 && displayedLevel.innerHTML !== "PRE INTERMEDIATE")
         {
-            fadeinText( displayedLevel, "PRE INTERMEDIATE" );
+            fadeinText(displayedLevel, "PRE INTERMEDIATE");
         }
-        else if( this.value >= 40 && this.value < 60 && displayedLevel.innerHTML !== "INTERMEDIATE" )
+        else if (this.value >= 40 && this.value < 60 && displayedLevel.innerHTML !== "INTERMEDIATE")
         {
-            fadeinText( displayedLevel, "INTERMEDIATE" );
+            fadeinText(displayedLevel, "INTERMEDIATE");
         }
-        else if( this.value >= 60 && this.value < 80 && displayedLevel.innerHTML !== "UPPER INTERMEDIATE" )
+        else if (this.value >= 60 && this.value < 80 && displayedLevel.innerHTML !== "UPPER INTERMEDIATE")
         {
-            fadeinText( displayedLevel, "UPPER INTERMEDIATE" );
+            fadeinText(displayedLevel, "UPPER INTERMEDIATE");
         }
-        else if( this.value >= 80 && displayedLevel.innerHTML !== "ADVANCED" )
+        else if (this.value >= 80 && displayedLevel.innerHTML !== "ADVANCED")
         {
-            fadeinText( displayedLevel, "ADVANCED" );
+            fadeinText(displayedLevel, "ADVANCED");
         }
-    } );
+    });
     /** ---------------------------
      * Sending message to content.js
      */
-    translate_btn.addEventListener( 'click', function ()
+    translate_btn.addEventListener('click', function ()
     {
-        chrome.tabs.query( {
-                               active: true,
-                               lastFocusedWindow: true
-                           }, function ( tabs )
-                           {
-                               // and use that tab to fill in out title and url
-                               let obj = {};
-                               obj.level = rangeInput.value;
-                               obj.langFrom = langFromBox.value;
-                               obj.langTo = langToBox.value;
-                               chrome.tabs.sendMessage( tabs[0].id, JSON.stringify( obj ) );
-                           } );
-    } );
+        chrome.tabs.query({
+            active: true,
+            lastFocusedWindow: true
+        }, function (tabs)
+        {
+            // and use that tab to fill in out title and url
+            let obj = {};
+            obj.level = rangeInput.value;
+            obj.langFrom = langFromBox.value;
+            obj.langTo = langToBox.value;
+            chrome.tabs.sendMessage(tabs[0].id, JSON.stringify(obj));
+        });
+    });
     /** ---------------------------
      * "do it" button customisation
      */
-    let loading = function ( e )
+    let loading = function (e)
     {
         e.preventDefault();
         e.stopPropagation();
-        e.target.classList.add( 'loading' );
-        e.target.setAttribute( 'disabled', 'disabled' );
-        setTimeout( function ()
-                    {
-                        e.target.classList.remove( 'loading' );
-                        e.target.removeAttribute( 'disabled' );
-                    }, 1500 );
+        e.target.classList.add('loading');
+        e.target.setAttribute('disabled', 'disabled');
+        setTimeout(function ()
+        {
+            e.target.classList.remove('loading');
+            e.target.removeAttribute('disabled');
+        }, 1500);
     };
 
-    let btns = document.querySelectorAll( 'button' );
-    for( let i = btns.length - 1; i >= 0; i-- )
+    let btns = document.querySelectorAll('button');
+    for (let i = btns.length - 1; i >= 0; i--)
     {
-        btns[i].addEventListener( 'click', loading );
+        btns[i].addEventListener('click', loading);
     }
 };
+
 /** ------------------------------------------------------- */
-function fadeinText( element, newValue )
+function fadeinText(element, newValue)
 {
-    element.classList.add( 'hide' );
-    setTimeout( function ()
-                {
-                    element.innerHTML = newValue;
-                }, 200 );
-    setTimeout( function ()
-                {
-                    element.classList.remove( 'hide' );
-                }, 500 );
+    element.classList.add('hide');
+    setTimeout(function ()
+    {
+        element.innerHTML = newValue;
+    }, 200);
+    setTimeout(function ()
+    {
+        element.classList.remove('hide');
+    }, 500);
 }
+
 /** ------------------------------------------------------- */
