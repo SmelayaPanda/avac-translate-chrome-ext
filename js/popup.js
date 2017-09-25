e5b7a7db2f9031771d59106d7f1a5f5a6835410a
panda <SmelayaPanda@mail.ru>
2017-09-26 01:04:57 +0700

    hide footer

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,4 @@
-window.onload = function ()
-{
+window.onload = function () {
     /** ------------
      * HTML elements
      */
@@ -13,90 +12,70 @@ window.onload = function ()
     /** ---------------------------
      * Setting Chrome storage value
      */
-    langFromBox.onchange = function ()
-    {
+    langFromBox.onchange = function () {
         chrome.storage.sync.set({'langFrom': langFromBox.value});
     };
-    langToBox.onchange = function ()
-    {
+    langToBox.onchange = function () {
         chrome.storage.sync.set({'langTo': langToBox.value});
     };
-    rangeInput.onchange = function ()
-    {
+    rangeInput.onchange = function () {
         chrome.storage.sync.set({'rangeInput': rangeInput.value});
         chrome.storage.sync.set({'displayedLevel': displayedLevel.innerText});
     };
-    onLoadCheckBox.onchange = function ()
-    {
-        if (onLoadCheckBox.checked)
-        {
+    onLoadCheckBox.onchange = function () {
+        if (onLoadCheckBox.checked) {
             chrome.storage.sync.set({'onLoadCheckBox': true});
         }
-        else
-        {
+        else {
             chrome.storage.sync.set({'onLoadCheckBox': false});
         }
     };
     /** ---------------------------
      * Getting Chrome storage value
      */
-    chrome.storage.sync.get('langFrom', function (obj)
-    {
+    chrome.storage.sync.get('langFrom', function (obj) {
         langFromBox.value = obj.langFrom ? obj.langFrom : 'eng';
     });
-    chrome.storage.sync.get('langTo', function (obj)
-    {
+    chrome.storage.sync.get('langTo', function (obj) {
         langToBox.value = obj.langTo ? obj.langTo : 'eng';
     });
-    chrome.storage.sync.get('rangeInput', function (obj)
-    {
+    chrome.storage.sync.get('rangeInput', function (obj) {
         rangeInput.value = obj.rangeInput ? obj.rangeInput : 0;
     });
-    chrome.storage.sync.get('displayedLevel', function (obj)
-    {
+    chrome.storage.sync.get('displayedLevel', function (obj) {
         displayedLevel.innerText = obj.displayedLevel ? obj.displayedLevel : 'WELCOME TO AVAC! ';
     });
-    chrome.storage.sync.get('onLoadCheckBox', function (obj)
-    {
-        if (obj.onLoadCheckBox)
-        {
+    chrome.storage.sync.get('onLoadCheckBox', function (obj) {
+        if (obj.onLoadCheckBox) {
             onLoadCheckBox.checked = true;
         }
     });
     /** ---------------------------------------------------- */
-    rangeInput.addEventListener('input', function ()
-    {
-        if (this.value < 20 && displayedLevel.innerHTML !== "BEGINNER")
-        {
-            fadeinText(displayedLevel, "BEGINNER");
+    rangeInput.addEventListener('input', function () {
+        if (this.value < 20 && displayedLevel.innerHTML !== "BEGINNER") {
+            fadeinElement(displayedLevel, "BEGINNER");
         }
-        else if (this.value >= 20 && this.value < 40 && displayedLevel.innerHTML !== "PRE INTERMEDIATE")
-        {
-            fadeinText(displayedLevel, "PRE INTERMEDIATE");
+        else if (this.value >= 20 && this.value < 40 && displayedLevel.innerHTML !== "PRE INTERMEDIATE") {
+            fadeinElement(displayedLevel, "PRE INTERMEDIATE");
         }
-        else if (this.value >= 40 && this.value < 60 && displayedLevel.innerHTML !== "INTERMEDIATE")
-        {
-            fadeinText(displayedLevel, "INTERMEDIATE");
+        else if (this.value >= 40 && this.value < 60 && displayedLevel.innerHTML !== "INTERMEDIATE") {
+            fadeinElement(displayedLevel, "INTERMEDIATE");
         }
-        else if (this.value >= 60 && this.value < 80 && displayedLevel.innerHTML !== "UPPER INTERMEDIATE")
-        {
-            fadeinText(displayedLevel, "UPPER INTERMEDIATE");
+        else if (this.value >= 60 && this.value < 80 && displayedLevel.innerHTML !== "UPPER INTERMEDIATE") {
+            fadeinElement(displayedLevel, "UPPER INTERMEDIATE");
         }
-        else if (this.value >= 80 && displayedLevel.innerHTML !== "ADVANCED")
-        {
-            fadeinText(displayedLevel, "ADVANCED");
+        else if (this.value >= 80 && displayedLevel.innerHTML !== "ADVANCED") {
+            fadeinElement(displayedLevel, "ADVANCED");
         }
     });
     /** ---------------------------
      * Sending message to content.js
      */
-    translate_btn.addEventListener('click', function ()
-    {
+    translate_btn.addEventListener('click', function () {
         chrome.tabs.query({
             active: true,
             lastFocusedWindow: true
-        }, function (tabs)
-        {
+        }, function (tabs) {
             // and use that tab to fill in out title and url
             let obj = {};
             obj.level = rangeInput.value;
@@ -108,36 +87,30 @@ window.onload = function ()
     /** ---------------------------
      * "do it" button customisation
      */
-    let loading = function (e)
-    {
+    let loading = function (e) {
         e.preventDefault();
         e.stopPropagation();
         e.target.classList.add('loading');
         e.target.setAttribute('disabled', 'disabled');
-        setTimeout(function ()
-        {
+        setTimeout(function () {
             e.target.classList.remove('loading');
             e.target.removeAttribute('disabled');
         }, 1500);
     };
 
     let btns = document.querySelectorAll('button');
-    for (let i = btns.length - 1; i >= 0; i--)
-    {
+    for (let i = btns.length - 1; i >= 0; i--) {
         btns[i].addEventListener('click', loading);
     }
 };
 
 /** ------------------------------------------------------- */
-function fadeinText(element, newValue)
-{
+function fadeinElement(element, newValue) {
     element.classList.add('hide');
-    setTimeout(function ()
-    {
+    setTimeout(function () {
         element.innerHTML = newValue;
     }, 200);
-    setTimeout(function ()
-    {
+    setTimeout(function () {
         element.classList.remove('hide');
     }, 500);
 }
--- js/unused.js
+++ js/unused.js
@@ -4,39 +4,36 @@
 
 // Not used now
 const url = "https://translate.yandex.net/api/v1.5/tr.json/translate",
-        keyAPI = "trnsl.1.1.20130922T110455Z.4a9208e68c61a760.f819c1db302ba637c2bea1befa4db9f784e9fbb8";
-function yandexTranslate()
-{
+    keyAPI = "trnsl.1.1.20130922T110455Z.4a9208e68c61a760.f819c1db302ba637c2bea1befa4db9f784e9fbb8";
+
+function yandexTranslate() {
 
     let xhr = new XMLHttpRequest(),
-            //textAPI = document.querySelector('#source').value,
-            textAPI = "Hello",
-            //langAPI = document.querySelector('#lang').value;
-            langAPI = "ru";
+        //textAPI = document.querySelector('#source').value,
+        textAPI = "Hello",
+        //langAPI = document.querySelector('#lang').value;
+        langAPI = "ru";
 
     //noinspection JSUndeclaredVariable
     data = "key=" + keyAPI + "&text=" + textAPI + "&lang=" + langAPI;
-    xhr.open( "POST", url, true );
-    xhr.setRequestHeader( "Content-type", "application/x-www-form-urlencoded" );
-    xhr.send( data );
-    xhr.onreadystatechange = function ()
-    {
-        if( this.readyState === 4 && this.status === 200 )
-        {
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send(data);
+    xhr.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
             let res = this.responseText;
-            let json = JSON.parse( res );
-            if( json.code === 200 )
-            {
+            let json = JSON.parse(res);
+            if (json.code === 200) {
                 return json.text[0];
                 //document.querySelector('#output').innerHTML = json.text[0];
             }
-            else
-            {
+            else {
 
-                console.log( "Error code:" + json.code );
+                console.log("Error code:" + json.code);
                 //document.querySelector('#output').innerHTML = "Error Code: " + json.code;
             }
         }
     }
 }
+
 // -------------------------------------------------------------------------
