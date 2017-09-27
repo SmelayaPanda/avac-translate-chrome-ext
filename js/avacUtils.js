3d6ddd2399b026a79b69c19f73bf4bca6263af43
panda <SmelayaPanda@mail.ru>
2017-09-28 01:10:36 +0700

    speak examples

--- js/avacUtils.js
+++ js/avacUtils.js
@@ -21,3 +21,23 @@ let fontLoader = function (param) {
 
     link.href = 'http://fonts.googleapis.com/css?family=' + param.familyt;
 };
+/** -----------------------------------------------------------------------------------
+ * Speak word use SpeechSynthesisUtterance
+ * @param word
+ * @param lang
+ * @param volume
+ * @param rate
+ * @param pitch
+ * @param voiceURI
+ **/
+
+function speakWord(word, lang, volume, rate, pitch, voiceURI) {
+    let msg = new SpeechSynthesisUtterance();
+    msg.voiceURI = voiceURI;
+    msg.pitch = pitch;      // 0 to 2
+    msg.volume = volume;    // volume, from 0 to 1, default is 1
+    msg.rate = rate;        // speaking rate, default is 1 (0.1 to 10)
+    msg.lang = lang;        // language, default is 'en-US'
+    msg.text = word;
+    window.speechSynthesis.speak(msg);
+}
--- js/footer.js
+++ js/footer.js
@@ -1,4 +1,44 @@
 /** --------------------------------------------------------------------------
+ * Create DOM element - footer and inject on page
+ **/
+function createFooterAvac() {
+    let footer = document.createElement('footer');
+    footer.id = 'footerAvac';
+    footer.innerHTML =
+        `<div>
+    <div id="emptyBlockAvac">
+    </div>
+    <div id="translatedWordAvac">
+        <button id="playWordBtnAvac" type="button">
+            <strong id="playWordAvac">Your word is here!</strong></button>
+    </div>
+    <div id="synonymsAvac"><span style="color: grey"> Synonyms </span>
+        <li class="synonymsAvac" id="avacSynonyms_1">yes one</li>
+        <li class="synonymsAvac" id="avacSynonyms_2">may be two</li>
+        <li class="synonymsAvac" id="avacSynonyms_3">and three</li>
+    </div>
+    <div id="sentenceExampleAvac">
+        Fool sentence with max priority! And bla and np bla, just relax, sentence is to large, and and and it'not all,
+        for test, stop, yes
+        Oh no, more more, words casha =) and hello world example
+    </div>
+</div>`;
+
+    document.body.appendChild(footer);
+
+    document.getElementById('sentenceExampleAvac').onclick = function () {
+        speakWord(document.getElementById('sentenceExampleAvac').innerText, 'en-GB', 0.8, 0.8, 1, 'native');
+    };
+
+    let synonyms = document.getElementsByClassName('synonymsAvac');
+    for (let s in synonyms) {
+        synonyms[s].onclick = function () {
+            speakWord(synonyms[s].innerText, 'en-GB', 0.8, 0.8, 1, 'native');
+        }
+    }
+}
+
+/** --------------------------------------------------------------------------
  * Create DOM element - closeFooter and inject on page
  **/
 function createCloseFooterAvac() {
@@ -20,34 +60,7 @@ function createCloseFooterAvac() {
     document.body.style.marginBottom = '10em';
     closeFooter.style.height = '2.5em'
 }
-/** --------------------------------------------------------------------------
- * Create DOM element - footer and inject on page
- **/
-function createFooterAvac() {
-    let footer = document.createElement('footer');
-    footer.id = 'footerAvac';
-    footer.innerHTML =
-        `<div>
-    <div id="emptyBlockAvac">
-    </div>
-    <div id="translatedWordAvac">
-        <button id="playWordBtnAvac" type="button" onclick="audioAvac.play()">
-            <strong id="playWordAvac">Your word is here!</strong></button>
-    </div>
-    <div id="synonymsAvac"> Synonyms
-        <li id="avacSynonyms_1">example one</li>
-        <li id="avacSynonyms_2">example two</li>
-        <li id="avacSynonyms_3">example three</li>
-    </div>
-    <div id="sentenceExampleAvac">
-        Fool sentence with max priority! And bla and np bla, just relax, sentence is to large, and and and it'not all,
-        for test, stop, yes
-        Oh no, more more, words casha =) and hello world example
-    </div>
-</div>`;
 
-    document.body.appendChild(footer);
-}
 /** --------------------------------------------------------------------------
  * Fade in and Out for content footer
  * @param footer
